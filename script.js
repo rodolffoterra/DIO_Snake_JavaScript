469b543826e0f94306add45c765f6c94a57fb623
Rodolfo Terra <rodolffoterra@hotmail.com>
2021-02-06 15:24:16 -0300

    ciramos as direções corretas

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'up') direction = 'down';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'down') direction = 'up';
+}
+
 function iniciarjogo() {
     criarBG();
     criarCobrinha();
