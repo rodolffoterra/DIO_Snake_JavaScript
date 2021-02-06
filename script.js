f60fab4943b1f9a3c2c0e53c3cc3248c9a39169f
Rodolfo Terra <rodolffoterra@hotmail.com>
2021-02-06 14:39:43 -0300

    criação dos movimento de nossa cobrinha

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box,
 }
 
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,6 +21,27 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
+function iniciarjogo() {
+    criarBG();
+    criarCobrinha();
 
-criarCobrinha();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    
+}
+
+let jogo = setInterval(IniciarJogo, 100);
