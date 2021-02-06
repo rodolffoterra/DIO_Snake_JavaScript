b0b5bde08883f9f1a40947df96644ee3a974a75f
Rodolfo Terra <rodolffoterra@hotmail.com>
2021-02-06 15:52:00 -0300

    definir a eatoriedade da frutinha e tamanho da cobrinha

--- script.js
+++ script.js
@@ -60,7 +60,12 @@ function iniciarJogo(){
     if (direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-        snake.pop()
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
     
         
     let newHead ={
